cdb09c9943466b7a5b6e61f78f6481698bfb8f02
PGL <42806449+PGL888999@users.noreply.github.com>
2020-01-23 23:28:45 +0800

    Delete exercise9.py

--- exercise9.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/python
-def move(n, a, var, c):
-    if(n == 1):
-        print(a,"->",c)
-    return
-move(n-1, a, c, var)
-move(1, a, var, c)
-move(n-1, var, a, c)
-move(3, "a", "b", "c")
